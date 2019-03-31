e768b993102c8e06982e74414ca8a19a12425693
Steven Joshua Klickermann <sjklickermann@gmail.com>
2019-03-31 01:30:42 -0400

    Add pathfinding function
    Added an A* pathfinding function. The trial case is the same as before, but
    now the path is not manually entered. Instead, getPath finds and returns the
    trial path.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <cstring>
 #include <cstdlib>
 #include <vector>
+#include <list>
+#include <algorithm>
 
 enum Direction { up, down, left, right };
 
@@ -11,90 +13,54 @@ struct Position {
     Direction facing;
 };
 
+bool operator==(const Position &lhs, const Position &rhs) {
+    if (lhs.row == rhs.row) {
+        if (lhs.column == rhs.column) {
+            if (lhs.facing == rhs.facing) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 struct PathNode {
     Position position;
     float F, G, H;
+    PathNode* parent;
 };
 
 // function prototypes
 char DirectionToChar (Direction dir);
 std::vector<Position> getValidMoves(Position current, char map[10][10]);
-float getHeuristicDistance (Position current, Position destination);
+float getHeuristicDistance(Position current, Position destination);
+std::vector<Position> getPath(Position start, Position end, char map[10][10]);
 
 int main() {
     // initial warehouse map
     // 'X' => wall, 'B' => bin, '.' => empty
-    const char init_map[10][10] = {{'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'},
-                                   {'X', '.', '.', '.', '.', '.', '.', '.', '.', 'X'},
-                                   {'X', '.', '.', 'B', 'B', '.', 'B', 'B', '.', 'X'},
-                                   {'X', '.', '.', 'B', 'B', '.', 'B', 'B', '.', 'X'},
-                                   {'X', '.', '.', 'B', 'B', '.', 'B', 'B', '.', 'X'},
-                                   {'X', '.', '.', '.', '.', '.', 'B', 'B', '.', 'X'},
-                                   {'X', '.', '.', 'B', 'B', '.', 'B', 'B', '.', 'X'},
-                                   {'X', '.', '.', 'B', 'B', '.', 'B', 'B', '.', 'X'},
-                                   {'X', '.', '.', '.', '.', '.', '.', '.', '.', 'X'},
-                                   {'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'}};
+    char init_map[10][10] = {{'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'},
+                             {'X', '.', '.', '.', '.', '.', '.', '.', '.', 'X'},
+                             {'X', '.', '.', 'B', 'B', '.', 'B', 'B', '.', 'X'},
+                             {'X', '.', '.', 'B', 'B', '.', 'B', 'B', '.', 'X'},
+                             {'X', '.', '.', 'B', 'B', '.', 'B', 'B', '.', 'X'},
+                             {'X', '.', '.', '.', '.', '.', 'B', 'B', '.', 'X'},
+                             {'X', '.', '.', 'B', 'B', '.', 'B', 'B', '.', 'X'},
+                             {'X', '.', '.', 'B', 'B', '.', 'B', 'B', '.', 'X'},
+                             {'X', '.', '.', '.', '.', '.', '.', '.', '.', 'X'},
+                             {'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'}};
     char display_map[10][10];
 
     // a test path => delete later
     std::vector<Position> path;
-    Position next;
-    next.row = 1;
-    next.column = 1;
-    next.facing = down;
-    path.push_back(next);
-    next.row = 2;
-    next.column = 1;
-    next.facing = down;
-    path.push_back(next);
-    next.row = 3;
-    next.column = 1;
-    next.facing = down;
-    path.push_back(next);
-    next.row = 4;
-    next.column = 1;
-    next.facing = down;
-    path.push_back(next);
-    next.row = 5;
-    next.column = 1;
-    next.facing = down;
-    path.push_back(next);
-    next.row = 5;
-    next.column = 1;
-    next.facing = right;
-    path.push_back(next);
-    next.row = 5;
-    next.column = 2;
-    next.facing = right;
-    path.push_back(next);
-    next.row = 5;
-    next.column = 3;
-    next.facing = right;
-    path.push_back(next);
-    next.row = 5;
-    next.column = 4;
-    next.facing = right;
-    path.push_back(next);
-    next.row = 5;
-    next.column = 5;
-    next.facing = right;
-    path.push_back(next);
-    next.row = 5;
-    next.column = 5;
-    next.facing = down;
-    path.push_back(next);
-    next.row = 6;
-    next.column = 5;
-    next.facing = down;
-    path.push_back(next);
-    next.row = 7;
-    next.column = 5;
-    next.facing = down;
-    path.push_back(next);
-    next.row = 7;
-    next.column = 5;
-    next.facing = left;
-    path.push_back(next);
+    Position start, end;
+    start.row = 1;
+    start.column = 1;
+    start.facing = down;
+    end.row = 7;
+    end.column = 5;
+    end.facing = left;
+    path = getPath(start, end, init_map);
 
     char c;
     for (std::vector<Position>::iterator it = path.begin(); it != path.end(); it++) {
@@ -178,7 +144,112 @@ std::vector<Position> getValidMoves(Position current, char map[10][10]) {
     return moves;
 }
 
-float getHeuristicDistance (Position current, Position destination) {
+float getHeuristicDistance(Position current, Position destination) {
     // will use simple manhattan distance, ignoring rotations too
     return float(abs(destination.row-current.row) + abs(destination.column-current.column));
+}
+
+std::vector<Position> getPath(Position start, Position end, char map[10][10]) {
+    std::list<PathNode> openList, closedList;
+    std::vector<Position> path;
+    PathNode temp;
+    Position temp_pos;
+    
+    // add starting node to open list
+    temp.position.row = start.row;
+    temp.position.column = start.column;
+    temp.position.facing = start.facing;
+    temp.parent = NULL;
+    temp.G = 0.0;
+    temp.H = getHeuristicDistance(start, end);
+    temp.F = temp.G + temp.H;
+    openList.push_back(temp);
+
+    while (true) {
+        // get current square and move to closed list
+        PathNode current;
+        std::list<PathNode>::iterator current_it = openList.begin();
+        current = *current_it;
+        for (std::list<PathNode>::iterator it = openList.begin(); it != openList.end(); it++) {
+            if ((*current_it).F > (*it).F) {
+                current_it = it;
+                current = *it;
+            }
+        }
+        closedList.push_back(current);
+        openList.erase(current_it);
+
+        // stop if the end node was just placed on the closed list (i.e. path found)
+        if (current.position == end) break;
+
+        // for each valid move from current position...
+        std::vector<Position> moves = getValidMoves(current.position, map);
+        bool onClosedList, onOpenList;
+        for (std::vector<Position>::iterator it = moves.begin(); it != moves.end(); it++) {
+            // check if the move is not on the closed list
+            onClosedList = false;
+            for (std::list<PathNode>::iterator closed_it = closedList.begin(); closed_it != closedList.end(); closed_it++) {
+                if ((*closed_it).position == (*it)) {
+                    onClosedList = true;
+                    break;
+                }
+            }
+            if (!onClosedList) {
+                // now check if the move is on the open list
+                std::list<PathNode>::iterator found_it;
+                onOpenList = false;
+                for (std::list<PathNode>::iterator open_it = openList.begin(); open_it != openList.end(); open_it++) {
+                    if ((*open_it).position == (*it)) {
+                        found_it = open_it;
+                        onOpenList = true;
+                        break;
+                    }
+                }
+                temp.position.row = (*it).row;
+                temp.position.column = (*it).column;
+                temp.position.facing = (*it).facing;
+                // Be careful here. Remember the scope of the "current" variable. What we really want is the address
+                // of the persistent PathNode representing current, which is stored at the end of closedList right now.
+                temp.parent = &(closedList.back());
+                temp.G = current.G + 1.0;
+                temp.H = getHeuristicDistance(temp.position, end);
+                temp.F = temp.G + temp.H;
+                if (!onOpenList) {
+                    // add move to open list, making current node the parent
+                    openList.push_back(temp);
+                } else {
+                    // check if path to this position is better than the path
+                    // used to get to this position, as stored on the open list.
+                    // Replace if better.
+                    if ((*found_it).G > temp.G) {
+                        openList.erase(found_it);
+                        openList.push_back(temp);
+                    }
+                }
+            }
+        }
+
+        // if the open list is empty, and we are here, then there were no new nodes
+        // to try and no path to destination was found
+        if (openList.empty()) break;
+
+    }
+
+    // Save the found path to the path vector. Start with the target square, going
+    // through the parents until start node is reached.
+    if (!openList.empty()) {
+        // If we are here, a path was found (open list was not empty), and the target
+        // node was just pushed onto the back of the closed list.
+        temp = closedList.back();
+        while (temp.parent != NULL) {
+            temp_pos = temp.position;
+            path.push_back(temp_pos);
+            temp = *(temp.parent);
+        }
+        temp_pos = temp.position;
+        path.push_back(temp_pos);
+        std::reverse(path.begin(), path.end());
+    }
+
+    return path;
 }
