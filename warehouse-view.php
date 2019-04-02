d3a2515773b6bacf53964a28bf15d2650b38cbb4
Steven Joshua Klickermann <sjklickermann@gmail.com>
2019-04-02 03:37:42 -0400

    Add default view of warehouse floor tiles

--- warehouse-view.php
+++ warehouse-view.php
@@ -1,8 +1,18 @@
 <html>
 	<head>
 		<title>Warehouse-View</title>
+		<link rel="stylesheet" href="warehouse-view.css"/>
 	</head>
 	<body>
-		<?php echo '<p>Unable to reach warehouse.</p>';?>
+		<?php echo "<p>Unable to reach warehouse.</p>"; ?>
+		<?php echo "<div class='warehouse'>"; ?>
+		<?php
+			for ($row=0; $row<10; $row++) {
+				for ($column=0; $column<10; $column++) {
+					echo "<div class='tile floor'></div>";
+				}
+			}
+		?>
+		<?php echo "</div>"; ?>
 	</body>
 </html>
