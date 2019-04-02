ca0abc4bc17a673f792e71611bb9e45656523872
Steven Joshua Klickermann <sjklickermann@gmail.com>
2019-04-02 04:01:25 -0400

    Add hard-coded warehouse layout

--- warehouse-view.php
+++ warehouse-view.php
@@ -7,9 +7,33 @@
 		<?php echo "<p>Unable to reach warehouse.</p>"; ?>
 		<?php echo "<div class='warehouse'>"; ?>
 		<?php
+			$warehouse =	array
+							(
+							array('X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'),
+							array('X', '.', '.', '.', '.', '.', '.', '.', '.', 'X'),
+							array('X', '.', '.', 'B', 'B', '.', 'B', 'B', '.', 'X'),
+							array('X', '.', '.', 'B', 'B', '.', 'B', 'B', '.', 'X'),
+							array('X', '.', '.', 'B', 'B', '.', 'B', 'B', '.', 'X'),
+							array('X', '.', '.', '.', '.', '.', 'B', 'B', '.', 'X'),
+							array('X', '.', '.', 'B', 'B', '.', 'B', 'B', '.', 'X'),
+							array('X', '.', '.', 'B', 'B', '.', 'B', 'B', '.', 'X'),
+							array('X', '.', '.', '.', '.', '.', '.', '.', '.', 'X'),
+							array('X', 'S', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X')
+							);
+			array_push($warehouse); 
 			for ($row=0; $row<10; $row++) {
 				for ($column=0; $column<10; $column++) {
-					echo "<div class='tile floor'></div>";
+					if ($warehouse[$row][$column] == '.') {
+						echo "<div class='tile floor'></div>";
+					} elseif ($warehouse[$row][$column] == 'X') {
+						echo "<div class='tile wall'></div>";
+					} elseif ($warehouse[$row][$column] == 'B') {
+						echo "<div class='tile bin'></div>";
+					} elseif ($warehouse[$row][$column] == 'S') {
+						echo "<div class='tile shipping'></div>";
+					} else {
+						echo "<div class='tile unknown'>?</div>";
+					}
 				}
 			}
 		?>
