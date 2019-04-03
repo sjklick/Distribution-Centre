4285974ce78b0f8d583916f0586f8201439354be
Steven Joshua Klickermann <sjklickermann@gmail.com>
2019-04-02 20:37:33 -0400

    Make demo test run without user input
    Using sleep_until, order pickers now update every second without
    user input (for development purposes).

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <cstdlib>
 #include <string>
 #include <sstream>
+#include <chrono>
+#include <thread>
 #include "position.hpp"
 #include "order-picker.hpp"
 
@@ -49,50 +51,63 @@ int main() {
     char display_map[10][10];
 	char path_map[10][10];
 
-    Position home, bin;
+	Position home[4];
+	Position bin[4];
 	const int numPickers = 4;
 	OrderPicker* picker[numPickers];
 
-    home.row = 1;
-    home.column = 1;
-    home.facing = right;
-    bin.row = 7;
-    bin.column = 4;
-    bin.facing = right;
-	picker[0] = new OrderPicker(home);
-	picker[0]->processItem(bin);
+    home[0].row = 1;
+    home[0].column = 1;
+    home[0].facing = right;
+    bin[0].row = 7;
+    bin[0].column = 4;
+    bin[0].facing = right;
+	picker[0] = new OrderPicker(home[0]);
+	picker[0]->processItem(bin[0]);
 
-	home.row = 2;
-    home.column = 1;
-    home.facing = right;
-    bin.row = 7;
-    bin.column = 6;
-    bin.facing = down;
-	picker[1] = new OrderPicker(home);
-	picker[1]->processItem(bin);
+	home[1].row = 2;
+    home[1].column = 1;
+    home[1].facing = right;
+    bin[1].row = 7;
+    bin[1].column = 6;
+    bin[1].facing = down;
+	picker[1] = new OrderPicker(home[1]);
+	picker[1]->processItem(bin[1]);
 
-	home.row = 3;
-    home.column = 1;
-    home.facing = right;
-    bin.row = 6;
-    bin.column = 6;
-    bin.facing = left;
-	picker[2] = new OrderPicker(home);
-	picker[2]->processItem(bin);
+	home[2].row = 3;
+    home[2].column = 1;
+    home[2].facing = right;
+    bin[2].row = 6;
+    bin[2].column = 6;
+    bin[2].facing = left;
+	picker[2] = new OrderPicker(home[2]);
+	picker[2]->processItem(bin[2]);
 
-	home.row = 4;
-    home.column = 1;
-    home.facing = right;
-    bin.row = 4;
-    bin.column = 7;
-    bin.facing = right;
-	picker[3] = new OrderPicker(home);
-	picker[3]->processItem(bin);
+	home[3].row = 4;
+    home[3].column = 1;
+    home[3].facing = right;
+    bin[3].row = 4;
+    bin[3].column = 7;
+    bin[3].facing = right;
+	picker[3] = new OrderPicker(home[3]);
+	picker[3]->processItem(bin[3]);
 
-	char c = ' ';
     Position current;
 	std::stringstream ss;
-    while (c != 'q') {
+    while (true) {
+
+		// TEMPORARY
+		// If all robots are idle, restart the process.
+		bool allIdle = true;
+		for (int i=0; i<numPickers; i++) {
+			if (picker[i]->getState() != State::idle) allIdle = false;
+		}
+		if (allIdle) {
+			for (int i=0; i<numPickers; i++) {
+				picker[i]->processItem(bin[i]);
+			}
+		}
+
         // map copy for display
         memcpy(display_map, init_map, 100);
         // add current picker positions to display map
@@ -117,8 +132,8 @@ int main() {
 			if (picker[i]->hasItem()) std::cout << " item=yes" << std::endl;
 			else std::cout << " item=no" << std::endl;
 		}
-        // wait for user to enter char
-        std::cin.get(c);
+        // Sleep for 1 second.
+        std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::seconds(1));
 		// map copy for path calculations
         memcpy(path_map, init_map, 100);
 		// add picker positions to path map
