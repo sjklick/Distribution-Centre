484084730ca4dbd851631121ccef3148d24c5def
Steven Joshua Klickermann <sjklickermann@gmail.com>
2019-04-07 22:28:17 -0400

    Add customer orders to SQL database
    Database "stock" now contains a table for customer order, a table
    for associated order items, and a table for the shipping and
    receiving items. Inserted two fictional customers for testing.

--- stock.sql
+++ stock.sql
@@ -16,58 +16,56 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `bins`
+-- Table structure for table `order_items`
 --
 
-DROP TABLE IF EXISTS `bins`;
+DROP TABLE IF EXISTS `order_items`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `bins` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `row` int(11) DEFAULT NULL,
-  `col` int(11) DEFAULT NULL,
-  `dir` char(1) DEFAULT NULL,
-  `nItems` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `bins`
---
-
-LOCK TABLES `bins` WRITE;
-/*!40000 ALTER TABLE `bins` DISABLE KEYS */;
-INSERT INTO `bins` VALUES (1,2,3,'l',4),(2,3,3,'l',3),(3,4,3,'l',6),(4,2,4,'r',9),(5,3,4,'r',4),(6,4,4,'r',4),(7,6,3,'l',9),(8,7,3,'l',9),(9,6,4,'r',6),(10,7,4,'r',9),(11,2,6,'u',6),(12,3,6,'l',7),(13,4,6,'l',7),(14,5,6,'l',8),(15,6,6,'l',7),(16,7,6,'d',6),(17,2,7,'u',2),(18,3,7,'r',5),(19,4,7,'r',4),(20,5,7,'r',9),(21,6,7,'r',4),(22,7,7,'d',6);
-/*!40000 ALTER TABLE `bins` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `items`
---
-
-DROP TABLE IF EXISTS `items`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `items` (
-  `bin` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
+CREATE TABLE `order_items` (
+  `order_id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
   `quantity` int(11) DEFAULT NULL,
-  PRIMARY KEY (`bin`,`name`),
-  KEY `fk_name` (`name`),
-  CONSTRAINT `fk_bin` FOREIGN KEY (`bin`) REFERENCES `bins` (`id`),
-  CONSTRAINT `fk_name` FOREIGN KEY (`name`) REFERENCES `products` (`name`)
+  PRIMARY KEY (`order_id`,`name`),
+  KEY `name` (`name`),
+  CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
+  CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`name`) REFERENCES `products` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `items`
+-- Dumping data for table `order_items`
 --
 
-LOCK TABLES `items` WRITE;
-/*!40000 ALTER TABLE `items` DISABLE KEYS */;
-INSERT INTO `items` VALUES (1,'apple',2),(1,'milk',2),(2,'apple',1),(2,'plate',2),(3,'apple',4),(3,'milk',2),(4,'banana',5),(4,'cup',4),(5,'banana',2),(5,'plate',2),(6,'banana',1),(6,'fork',3),(7,'chair',4),(7,'fork',5),(8,'chair',6),(8,'milk',3),(9,'plate',3),(9,'table cloth',3),(10,'apple',4),(10,'banana',2),(10,'bread',3),(11,'beef',2),(11,'carrot',4),(12,'carrot',2),(12,'chicken',5),(13,'beef',5),(13,'milk',2),(14,'cup',5),(14,'table cloth',3),(15,'bread',2),(15,'grapes',5),(16,'carrot',3),(16,'table',3),(17,'carrot',1),(17,'table cloth',1),(18,'grapes',5),(19,'cup',2),(19,'table',2),(20,'bread',4),(20,'cup',3),(20,'knife',2),(21,'chair',1),(21,'knife',3),(22,'apple',1),(22,'knife',3),(22,'milk',2);
-/*!40000 ALTER TABLE `items` ENABLE KEYS */;
+LOCK TABLES `order_items` WRITE;
+/*!40000 ALTER TABLE `order_items` DISABLE KEYS */;
+INSERT INTO `order_items` VALUES (1,'apple',3),(1,'carrot',2),(1,'cup',1),(2,'cup',1),(2,'fork',1),(2,'knife',1),(2,'plate',1),(2,'table',1),(2,'table cloth',1);
+/*!40000 ALTER TABLE `order_items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL AUTO_INCREMENT,
+  `customer` varchar(50) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (1,'Test',NULL),(2,'Test2',NULL);
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -95,6 +93,109 @@ LOCK TABLES `products` WRITE;
 INSERT INTO `products` VALUES ('apple',NULL,'food',12),('banana',NULL,'food',10),('beef',NULL,'food',7),('bread',NULL,'food',9),('carrot',NULL,'food',10),('chair',NULL,'furniture',11),('chicken',NULL,'food',5),('cup',NULL,'symbel',14),('fork',NULL,'symbel',8),('grapes',NULL,'food',10),('knife',NULL,'symbel',8),('milk',NULL,'food',11),('plate',NULL,'symbel',7),('table',NULL,'furniture',5),('table cloth',NULL,'symbel',7);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `receiving_items`
+--
+
+DROP TABLE IF EXISTS `receiving_items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `receiving_items` (
+  `name` varchar(50) NOT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  PRIMARY KEY (`name`),
+  CONSTRAINT `receiving_items_ibfk_1` FOREIGN KEY (`name`) REFERENCES `products` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `receiving_items`
+--
+
+LOCK TABLES `receiving_items` WRITE;
+/*!40000 ALTER TABLE `receiving_items` DISABLE KEYS */;
+/*!40000 ALTER TABLE `receiving_items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `shipping_items`
+--
+
+DROP TABLE IF EXISTS `shipping_items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `shipping_items` (
+  `name` varchar(50) NOT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  PRIMARY KEY (`name`),
+  CONSTRAINT `shipping_items_ibfk_1` FOREIGN KEY (`name`) REFERENCES `products` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shipping_items`
+--
+
+LOCK TABLES `shipping_items` WRITE;
+/*!40000 ALTER TABLE `shipping_items` DISABLE KEYS */;
+/*!40000 ALTER TABLE `shipping_items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stock_bins`
+--
+
+DROP TABLE IF EXISTS `stock_bins`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `stock_bins` (
+  `bin_id` int(11) NOT NULL AUTO_INCREMENT,
+  `row` int(11) DEFAULT NULL,
+  `col` int(11) DEFAULT NULL,
+  `dir` char(1) DEFAULT NULL,
+  `nItems` int(11) DEFAULT NULL,
+  PRIMARY KEY (`bin_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stock_bins`
+--
+
+LOCK TABLES `stock_bins` WRITE;
+/*!40000 ALTER TABLE `stock_bins` DISABLE KEYS */;
+INSERT INTO `stock_bins` VALUES (1,2,3,'l',4),(2,3,3,'l',3),(3,4,3,'l',6),(4,2,4,'r',9),(5,3,4,'r',4),(6,4,4,'r',4),(7,6,3,'l',9),(8,7,3,'l',9),(9,6,4,'r',6),(10,7,4,'r',9),(11,2,6,'u',6),(12,3,6,'l',7),(13,4,6,'l',7),(14,5,6,'l',8),(15,6,6,'l',7),(16,7,6,'d',6),(17,2,7,'u',2),(18,3,7,'r',5),(19,4,7,'r',4),(20,5,7,'r',9),(21,6,7,'r',4),(22,7,7,'d',6);
+/*!40000 ALTER TABLE `stock_bins` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stock_items`
+--
+
+DROP TABLE IF EXISTS `stock_items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `stock_items` (
+  `bin_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  PRIMARY KEY (`bin_id`,`name`),
+  KEY `fk_name` (`name`),
+  CONSTRAINT `fk_bin` FOREIGN KEY (`bin_id`) REFERENCES `stock_bins` (`bin_id`),
+  CONSTRAINT `fk_name` FOREIGN KEY (`name`) REFERENCES `products` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stock_items`
+--
+
+LOCK TABLES `stock_items` WRITE;
+/*!40000 ALTER TABLE `stock_items` DISABLE KEYS */;
+INSERT INTO `stock_items` VALUES (1,'apple',2),(1,'milk',2),(2,'apple',1),(2,'plate',2),(3,'apple',4),(3,'milk',2),(4,'banana',5),(4,'cup',4),(5,'banana',2),(5,'plate',2),(6,'banana',1),(6,'fork',3),(7,'chair',4),(7,'fork',5),(8,'chair',6),(8,'milk',3),(9,'plate',3),(9,'table cloth',3),(10,'apple',4),(10,'banana',2),(10,'bread',3),(11,'beef',2),(11,'carrot',4),(12,'carrot',2),(12,'chicken',5),(13,'beef',5),(13,'milk',2),(14,'cup',5),(14,'table cloth',3),(15,'bread',2),(15,'grapes',5),(16,'carrot',3),(16,'table',3),(17,'carrot',1),(17,'table cloth',1),(18,'grapes',5),(19,'cup',2),(19,'table',2),(20,'bread',4),(20,'cup',3),(20,'knife',2),(21,'chair',1),(21,'knife',3),(22,'apple',1),(22,'knife',3),(22,'milk',2);
+/*!40000 ALTER TABLE `stock_items` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -105,4 +206,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-04-07 11:26:38
+-- Dump completed on 2019-04-07 22:27:50
